761aa982684313e4dafa02f9bc06deca7d430810
mkyu0917 <mkyu0917@gmail.com>
2026-06-02 09:20:28 +0900

    제곱근 구하고 곱하기

---
+++ src/com/algorithm/programers/sqrtMutilply.java
@@ -0,0 +1,44 @@
+package com.algorithm.programers;
+
+public class sqrtMutilply {
+
+    static void sqrtMultiply(long num){
+
+        long answer =0;
+
+        double sqrtNum=Math.sqrt(num);
+        if(sqrtNum%1==0){
+            double number=Math.pow(sqrtNum+1,2);
+            answer = Math.round(number);
+
+        } else {
+            answer=-1;
+        }
+
+        System.out.println(answer);
+    }
+
+    static void sqrtMultiply2(long num){
+
+        long answer = 0;
+
+        int sqrtNum = (int)Math.sqrt(num);
+
+        if((sqrtNum * sqrtNum) == num){
+            sqrtNum=sqrtNum+1;
+            answer = sqrtNum * sqrtNum;
+        }else{
+            answer = -1;
+        }
+
+
+        System.out.println(answer);
+    }
+
+
+    public static void main(String[] args) {
+
+        sqrtMultiply(121);
+        sqrtMultiply2(121);
+    }
+}
