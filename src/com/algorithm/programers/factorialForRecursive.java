bfd93ad237d5df8cb5f6740a4fa1279ff88b928f
mkyu0917 <mkyu0917@gmail.com>
2026-06-02 09:39:00 +0900

    재귀 팩토리얼

---
+++ src/com/algorithm/programers/factorialForRecursive.java
@@ -0,0 +1,25 @@
+package com.algorithm.programers;
+
+import java.io.IOException;
+
+public class factorialForRecursive {
+
+    static int factorial (int n){
+
+                if(n==0){
+                    return 1;
+                }else if(n<0){
+                    throw new IllegalArgumentException("잘못된 인수");
+                }
+
+        return n * factorial(n-1);
+    }
+
+    public static void main(String[] args) {
+
+        // System.out.println(factorial(5));
+        System.out.println(factorial(-1));
+
+    }
+
+}
